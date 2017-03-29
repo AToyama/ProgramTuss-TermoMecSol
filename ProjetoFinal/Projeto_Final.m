d980617d08204c9202f15987d9668b7a4a2c67b9
Lucas Scarlato Astur <lucassa3@al.insper.edu.br>
2017-03-29 19:06:47 -0300

    automatized Pg matrix with boundary conditions applied. Deformation and Tension for each bar not working properly

--- ProjetoFinal/Projeto_Final.m
+++ ProjetoFinal/Projeto_Final.m
@@ -2,7 +2,9 @@
 %
 %   @DESCRIPTION Analysis of 2D frames structures. @
 %                Disciplina: TRANSFERÊNCIAS DE CALOR E MECÂNICA DOS SÓLIDOS - 5ENGCOM 2017/1
-%                Aluno: Felipe Frid Buniac
+%                Aluno: Felipe Frid Buniac, Lucas Scarlato Astur, André De
+%                Marco Toyama e João Pedro Pieroni De Castro
+%     
 %                Professor: Caio Fernandes Rodrigo dos Santos
 
 %% Clean memory and close windows
@@ -84,11 +86,11 @@ fprintf(1,'\nREADING ELEMENT INCIDENCES...');
 if (FindKeyWord(kwIncid, File) == 0)
     warning(message, 'Keyword |%s| Not Found.', kwIncid);
 else
-    incidencia = (fscanf(File, '%d%f%f', [3 TotalNumElements]))';
+    Incidencia = (fscanf(File, '%d%f%f', [3 TotalNumElements]))';
 end
 
 % deletes element numbers
-incidencia(:, 1) = [];
+Incidencia(:, 1) = [];
 
 % Tipo de elemento
 ElemType = zeros(TotalNumElements,1);
@@ -124,7 +126,7 @@ if (FindKeyWord(kwGPs, File) == 0)
     warning(message, 'Keyword |%s| Not Found.', kwGPs);
 else
     NumGPs = fscanf(File, '%d', 1);
-    propriedades    = (fscanf(File, '%f%f%f', [1 NumGPs]))';
+    Propriedades    = (fscanf(File, '%f%f%f', [1 NumGPs]))';
 end
 
 %% Reading boundary conditions
@@ -155,81 +157,93 @@ end
 fclose(File);
 
 %%
-Num_Elem=length(incidencia(:,1));%pegando todas as linhas e 1 coluna de incidencia isso define o numero de elementos(elementos=barras)
 
 
-incidencia2=zeros(Num_Elem,5);%definindo o tamanho da matriz de incidencia global, inicializando com tudo zero
-K= zeros(length(coords(:,1)),length(coords(:,1)));%Inicializando a Matriz de Rigidez(4x4)
-Kg=zeros(length(coords(:,1))*2,length(coords(:,1))*2);%Inicializando a Matriz de Rigidez Global(8x8)
+Num_Elem = length(Incidencia(:,1));%pegando todas as linhas e 1 coluna de Incidencia isso define o numero de elementos(elementos=barras)
+Num_GDL = length(coords(:,1))*2;
+
+
+Incidencia2 = zeros(Num_Elem,5);%definindo o tamanho da matriz de Incidencia global, inicializando com tudo zero
+K = zeros(length(coords(:,1)),length(coords(:,1)));%Inicializando a Matriz de Rigidez(4x4)
+Kg = zeros(length(coords(:,1))*2,length(coords(:,1))*2);%Inicializando a Matriz de Rigidez Global(8x8)
 GDL = zeros(Num_Elem,4);%Inicializando a Matriz de GDL(6x4)
 
-%%
-%Reescrevendo a matriz de incidencia  com o COMPRIMENTO, COS e SEN
-for i=1:Num_Elem%for rodando de 1 até o numero de elementos da matriz de incidencia (todas as linhas da primeira coluna)
+
+for i=1:Num_Elem%for rodando de 1 até o numero de elementos da matriz de Incidencia (todas as linhas da primeira coluna)
     %pegando a cordenada de cada nó
-    %obtem a cordenada x do primeiro nó do elemento i
-    x1=coords(incidencia(i,1),1);%x1=pega a linha i da primeira coluna da matriz de incidencia e pego a posição (1,1) da matriz coords
-    %obtem a cordenada y do primeiro nó do elemento i
-    y1=coords(incidencia(i,1),2);%y1=pega a linha i da primeira coluna da matriz de incidencia
-    x2=coords(incidencia(i,2),1);%x2=pega a linha i da segunda coluna da matriz de incidencia
-    y2=coords(incidencia(i,2),2);%y2=pega a linha i da segunda coluna da matriz de incidencia
     
-    comprimento=sqrt(((x1-x2)^2) + ((y2-y1)^2));%definição do comprimento para cada elemento
+    x1 = coords(Incidencia(i,1),1);%x1=pega a linha i da primeira coluna da matriz de Incidencia e pego a posição (1,1) da matriz coords E obtem a cordenada x do primeiro nó do elemento i
+    y1 = coords(Incidencia(i,1),2);%y1=pega a linha i da primeira coluna da matriz de Incidencia E obtem a cordenada y do primeiro nó do elemento i
+    x2 = coords(Incidencia(i,2),1);%x2=pega a linha i da segunda coluna da matriz de Incidencia
+    y2 = coords(Incidencia(i,2),2);%y2=pega a linha i da segunda coluna da matriz de Incidencia
     
-    GDL(i,1) = incidencia(i,1)*2-1;
-    GDL(i,2) = incidencia(i,1)*2;
-    GDL(i,3) = incidencia(i,2)*2-1;
-    GDL(i,4) = incidencia(i,2)*2;
-     
-    cos= (x2-x1) /comprimento ;%definição de cos
-    sen= (y2-y1) /comprimento ;%definição de sen
+    Comprimento = sqrt(((x1-x2)^2) + ((y2-y1)^2));%definição do Comprimento para cada elemento
     
-    incidencia2(i,:)=[incidencia(i,1:2),comprimento, cos, sen];%Matriz nova de incidencia envolvendo a matriz inteira antiga de incidencia +comprimento, sen e cos
+    GDL(i,1) = Incidencia(i,1)*2-1;
+    GDL(i,2) = Incidencia(i,1)*2;
+    GDL(i,3) = Incidencia(i,2)*2-1;
+    GDL(i,4) = Incidencia(i,2)*2;
     
     
-    K(:,:,i)=((material(i,1)*propriedades(i)/comprimento))*[(cos^2) (cos*sen) (-(cos^2)) (-(cos*sen)); 
+    cos = (x2-x1) /Comprimento ;%definição de cos
+    sen = (y2-y1) /Comprimento ;%definição de sen
+    
+    Incidencia2(i,:) = [Incidencia(i,1:2),Comprimento, cos, sen];%Matriz nova de Incidencia envolvendo a matriz inteira antiga de Incidencia +Comprimento, sen e cos
+    
+    
+    K(:,:,i) = ((material(i,1)*Propriedades(i)/Comprimento))*[(cos^2) (cos*sen) (-(cos^2)) (-(cos*sen)); 
                                                             (cos*sen) (sen^2) (-(sen*cos)) (-(sen^2)); 
                                                             (-(cos^2)) (-(sen*cos)) (cos^2) (cos*sen);
-                                                            (-(sen*cos)) (-(sen^2)) (cos*sen) (sen^2)];%((material(i)*propriedades(i)/comprimento))= E*A/l*[]
-%(:,:,i)salvando todos os elementos da matriz K 4x4 na celula i
-%Isso me devolve as 6 matrizes de rigidez, uma para cada barra.
+                                                            (-(sen*cos)) (-(sen^2)) (cos*sen) (sen^2)];%((material(i)*Propriedades(i)/Comprimento))= E*A/l*[]
+
     Kg(GDL(i,:),GDL(i,:))=Kg(GDL(i,:),GDL(i,:))+(K(:,:,i));
-    %Matriz de rigidez global para a a estrutura(GDL de cada barra + K(matriz de rigidez) de cada barra)
-% Indexando com o vetor de GtrDL uma matriz Kg -- Kg(1 1) = Kg(1 1)+K(i)--
-%                                                  2 2       2 2
-%                                                  3 3       3 3
-%                                                  4 4       4 4
+                                        
 end
+
+Pg = zeros(Num_GDL,1);
+for i = 1:length(Loads(:,1)); %monta vetor de forças globais Pg
+    if Loads(i,2) == 1
+        Pg(Loads(i,1)*2-1,1) = Loads(i,3);   
+    else
+        Pg(Loads(i,1)*2,1) = Loads(i,3);
+    end
+end
+
+
+index = length(HDBCNodes(:,1));
+while index >= 1; %aplica condições de contorno e deleta linhas e colunas travadas da Kg e do vetor de forças Pg
+    if HDBCNodes(index,2) == 1
+        Kg(HDBCNodes(index,1)*2-1,:) = [];
+        Kg(:,HDBCNodes(index,1)*2-1) = [];
+        Pg(HDBCNodes(index,1)*2-1,:) = [];
+        index = index - 1;
+    else
+        Kg(HDBCNodes(index,1)*2,:) = [];
+        Kg(:,HDBCNodes(index,1)*2) = [];
+        Pg(HDBCNodes(index,1)*2,:) = [];
+        index = index - 1;
+    end    
+end
+
+
+
+
 
 %Montagem do vetor de carga global ( -1000 pois a força é para baixo)
 %(posição 8 pois incide no GDL 8)
-f = [0 0 0 0 0 0 0 -1000]'; % ' faz a transposição do vetor
-u = inv(Kg(5:8,5:8))*f(5:8,1); % u é meu solver u=(Kg^-1)*f (Aplicadas as condições de contorno.)
+
+U = inv(Kg)*Pg; % u é meu solver u=(Kg^-1)*f (Aplicadas as condições de contorno.)
 % u para os graus de liberdade livre(u=deslocamento)
 % Kg(5:8,5:8)Pega as linhas de 5 a 8 da coluna 5 a 8 de Kg e f(5:8,1) pega
-% as linhas 5 a 8 da coluna 1 de f.
+% as linhas 5 a 8 da coluna 1 de f
 ug = zeros(8,1);%u2= inv(Kg(1:4,1:4))*f(1:4,1);
-ug(5:8,1)=u;%ug contem agora os nos com valor 0 que são restritos e os nós livres que estavam em u 
+ug=U;%ug contem agora os nos com valor 0 que são restritos e os nós livres que estavam em u 
 
 
 %% Aplicando as Equações
 %Deformação e Tensão
-for i =1:Num_Elem
-    Deform(i)=(1/incidencia2(i,3))*[-(incidencia2(i,4)) -(incidencia2(i,5)) incidencia2(i,4) incidencia2(i,5)]*ug(GDL(i,:));
-    Tensao(i)=(material(i,1)/incidencia2(i,3))*[-(incidencia2(i,4)) -(incidencia2(i,5)) incidencia2(i,4) incidencia2(i,5)]*ug(GDL(i,:));
-end
 
-aux2 = 0;
-for i=1:4
-    if HDBCNodes(i,2) == 1
-        Kg(HDBCNodes(i,1)*2-1,:) = 0;
-        Kg(:,HDBCNodes(i,1)*2-1) = 0;
-    else
-        Kg(HDBCNodes(i,1)*2,:) = 0;
-        Kg(:,HDBCNodes(i,1)*2) = 0;    
-    end    
-end
-
-%GDL = zeros(length(coords),2); numero de nós por graus de liberdade e
-%fazer um for colocando 1 caso tenha condição de contorno(restrição)
-%GDL = zeros(length(incidencia),length(coords)); 
+for i = 1:Num_Elem
+    Deform(i)=(1/Incidencia2(i,3))*[-(Incidencia2(i,4)) -(Incidencia2(i,5)) Incidencia2(i,4) Incidencia2(i,5)]*U(GDL(i,:));
+    Tensao(i)=(material(i,1)/Incidencia2(i,3))*[-(Incidencia2(i,4)) -(Incidencia2(i,5)) Incidencia2(i,4) Incidencia2(i,5)]*U(GDL(i,:));
+end 
